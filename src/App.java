f184ae0026f963995c691f64b14195dfe0d85a84
Cristina <cristinaeliloja12@gmail.com>
2024-10-16 12:56:57 -0500

    Practica Busqueda Binaria

--- src/App.java
+++ src/App.java
@@ -3,7 +3,7 @@ import java.util.Comparator;
 import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Ingrese la cantidad de personas:");
         int n = scanner.nextInt();
